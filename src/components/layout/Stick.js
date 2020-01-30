8d38d28230f73e3a12c5c6dc35683ac0df954533
Jaime Duque <jaime.duque@estratek.com.co>
2020-01-29 22:13:56 -0500

    Header component is created

---
+++ src/components/layout/Stick.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Stick = () => {
+    return ( 
+        <header className="app-header">
+            <p className="name-user">Hello<span> Jaime</span></p>
+            <nav className="nav-main">
+                <a href="#!">Cerrar Sesión</a>
+            </nav>
+        </header>
+     );
+}
+ 
+export default Stick;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebar from '../layout/Sidebar';
+import Stick from '../layout/Stick';
 
 const Projects = () => {
     return ( 
         <div className="container-app">
             <Sidebar />
             <div className="main-section">
+                <Stick />
                 <main>
                     <div className="container-tasks"></div>
                 </main>
